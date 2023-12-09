8120ebdee6bf107288701f8e4bb2c516eafbe748
Muhammad Hannan <mdhannan.info@gmail.com>
2023-12-09 23:34:44 +0600

    Done navbar

--- react-admin/src/components/Navbar/Navbar.jsx
+++ react-admin/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import './Navbar.scss'
 import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
@@ -6,8 +7,6 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import CommentIcon from '@mui/icons-material/Comment';
 import ListIcon from '@mui/icons-material/List';
 import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
-
-
 const Navbar = () => {
     return (
         <div className='navbar'>
@@ -18,25 +17,32 @@ const Navbar = () => {
                 </div>
                 <div className='items'>
                     <div className='item'>
-                        <LanguageIcon />
+                        <LanguageIcon className='icon' />
                         English
                     </div>
                     <div className='item'>
-                        <DarkModeIcon />
+                        <DarkModeIcon className='icon' />
+                    </div>
+                   
+                    <div className='item'>
+                        <ZoomOutMapIcon className='icon' />
                     </div>
                     <div className='item'>
-                        <NotificationsNoneIcon />
-                    </div>
-                    <div className='item'>
-                        <ZoomOutMapIcon />
-                    </div>
-                    <div className='item'>
-                        <CommentIcon />
-                    </div>
-                    <div className='item'>
-                        <ListIcon />
+                        <NotificationsNoneIcon className='icon' />
+                        <div className='counter'>2</div>
                     </div>
 
+                    <div className='item'>
+                        <CommentIcon className='icon' />
+                        <div className='counter'>2</div>
+                    </div>
+                    <div className='item'>
+                        <ListIcon className='icon' />
+                    </div>
+                    
+                    <div className='item'>
+                        <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" alt="Avatar" className='avatar'/>
+                    </div>
 
                 </div>
             </div>
