0e7ae958bb20e4ecd623857212460c10c4f49af3
Muhammad Hannan <mdhannan.info@gmail.com>
2023-12-10 01:20:24 +0600

    Added next project

---
+++ nextjs-crush/src/app/about/page.js
@@ -0,0 +1,8 @@
+import styles from './about.module.css'
+export default function About() {
+    return (
+        <div className={styles.aboutSection}>
+            <h1 className={styles.title}>About page</h1>
+        </div>
+    )
+}
