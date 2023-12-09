c108cf87a872c206ca6994c54809bf3fcbab65c0
Muhammad Hannan <mdhannan.info@gmail.com>
2023-12-09 13:40:27 +0600

    Update navbar

--- react-admin/src/components/Navbar/Navbar.jsx
+++ react-admin/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
 import './Navbar.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import CommentIcon from '@mui/icons-material/Comment';
+import ListIcon from '@mui/icons-material/List';
+import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
+
+
 const Navbar = () => {
     return (
-        <h1 className="navbar">Navbar</h1>
+        <div className='navbar'>
+            <div className='wrapper'>
+                <div className='search'>
+                    <input type="text" name="search" />
+                    <SearchIcon />
+                </div>
+                <div className='items'>
+                    <div className='item'>
+                        <LanguageIcon />
+                        English
+                    </div>
+                    <div className='item'>
+                        <DarkModeIcon />
+                    </div>
+                    <div className='item'>
+                        <NotificationsNoneIcon />
+                    </div>
+                    <div className='item'>
+                        <ZoomOutMapIcon />
+                    </div>
+                    <div className='item'>
+                        <CommentIcon />
+                    </div>
+                    <div className='item'>
+                        <ListIcon />
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     );
 };
 
--- react-admin/src/pages/Home/Home.jsx
+++ react-admin/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../components/Navbar/Navbar';
 import './home.scss'
 const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
             <div className='home-container'>
-                <h2>Home Container</h2>
+                <Navbar/>
             </div>
         </div>
     );
