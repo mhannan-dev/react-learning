8a7652c7596edb5a1f9b75839e7b0b542752cee7
Muhammad Hannan <mdhannan.info@gmail.com>
2023-12-12 00:40:39 +0600

    Design card for blog

---
+++ blog-anisul/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <h1 className="mainHeading">Todo App</h1>
+      <div className="card">
+        <h3 className="heading">Card Title</h3>
+        <p className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quibusdam assumenda cumque voluptatibus placeat qui ratione? Voluptates accusantium ratione incidunt laboriosam perspiciatis error similique cum quibusdam dolor. Magni, fugiat porro?</p>
+        <p className="footerText strong">December 12, 2023</p>
+      </div>
+      
+      <div className="card">
+        <h3 className="heading">Card Title</h3>
+        <p className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quibusdam assumenda cumque voluptatibus placeat qui ratione? Voluptates accusantium ratione incidunt laboriosam perspiciatis error similique cum quibusdam dolor. Magni, fugiat porro?</p>
+        <p className="footerText strong">December 12, 2023</p>
+      </div>
+      
+      <div className="card">
+        <h3 className="heading">Card Title</h3>
+        <p className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quibusdam assumenda cumque voluptatibus placeat qui ratione? Voluptates accusantium ratione incidunt laboriosam perspiciatis error similique cum quibusdam dolor. Magni, fugiat porro?</p>
+        <p className="footerText strong">December 12, 2023</p>
+      </div>
+      
+      <div className="card">
+        <h3 className="heading">Card Title</h3>
+        <p className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quibusdam assumenda cumque voluptatibus placeat qui ratione? Voluptates accusantium ratione incidunt laboriosam perspiciatis error similique cum quibusdam dolor. Magni, fugiat porro?</p>
+        <p className="footerText strong">December 12, 2023</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- nextjs-crush/src/app/about/page.js
+++
@@ -1,8 +0,0 @@
-import styles from './about.module.css'
-export default function About() {
-    return (
-        <div className={styles.aboutSection}>
-            <h1 className={styles.title}>About page</h1>
-        </div>
-    )
-}
