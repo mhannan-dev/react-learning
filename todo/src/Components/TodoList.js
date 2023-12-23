205001b1176e2520a1e3cce60ce7b5f1a84ee2bf
Muhammad Hannan <mdhannan.info@gmail.com>
2023-12-23 22:04:49 +0600

    Update code

--- todo/src/Components/TodoList.js
+++ todo/src/Components/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Todo from './Todo';
 
 const TodoList = (props) => {
+  const reversedTodoList = [...props.todoList].reverse();
   return (
     <section>
       {props.todoList.map(todo => (
